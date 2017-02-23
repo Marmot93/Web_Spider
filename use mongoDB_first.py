944b950c6915769136d514dd8cd93cec187755f2
Marmot93 <654332905@qq.com>
2017-02-23 16:15:13 +0800

    1

--- use mongoDB_first.py
+++ use mongoDB_first.py
@@ -1,5 +1,8 @@
 import pymongo
 
+# 1.对接数据库 2.创建数据文件夹 3，创建数据文件
+# 4.读取文件加工保存到字典中 5，数据文件.insert_one(字典)，往数据文件中装数据
+
 client = pymongo.MongoClient('localhost',27017)     #  接口到mongodb 本地服务器locoalhost 27017
 walden = client['walden']                      # 创建标签瓦尔登
 sheet_tab = walden['sheet_tab']               # 在瓦尔登里创建sheet_line  在什么里面创建对象就是方括号字符串
