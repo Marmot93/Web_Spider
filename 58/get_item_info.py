47d5c27db105dbc8729442ba5177c5ee25d930ff
Marmot93 <654332905@qq.com>
2017-02-24 17:53:56 +0800

    finish

--- 58/get_item_info.py
+++ 58/get_item_info.py
@@ -7,5 +7,11 @@ wuba = client['wuba']
 url_list = wuba['url_list']
 item_info = wuba['ietm_info']
 
-for url in url_list.find() :
-    get_item_info(url['url'])
+def get_all_info():
+    for url in url_list.find() :
+        get_item_info(url['url'])
+
+
+if __name__ == '__main__':
+    pool = Pool()     #多进程（process = num）
+    get_all_info()
