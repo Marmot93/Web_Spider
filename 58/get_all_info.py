a2ea97e86dadc8b7e28ec3f391ccb2a908a9b4ed
Marmot93 <654332905@qq.com>
2017-02-26 22:27:06 +0800

    断点续传

--- 58/get_all_info.py
+++ 58/get_all_info.py
@@ -8,10 +8,14 @@ url_list = wuba['url_list']
 item_info = wuba['ietm_info']
 
 
-list = []
-for url in url_list.find():
-    list.append(url['url'])
+finded_url = [item['link'] for item in item_info.find()]
+all_url = [item['url'] for item in url_list.find()]
+
+lest_url = set(all_url) - set(finded_url)
+
 
 if __name__ == '__main__':
     pool = Pool()     #多进程（process = num）
-    pool.map(get_item_info,list)
+    pool.map(get_item_info,lest_url)
+    pool.close()
+    pool.join()
