e67a4ebe2f46647fdd4ee3a53240ca6c53bbf7f7
Marmot93 <654332905@qq.com>
2017-03-03 19:06:05 +0800

    断点续传

---
+++ Xapth/spider_Xpath.py
@@ -0,0 +1,23 @@
+import requests
+from lxml import etree
+
+# 用xpath重构58详细信息抓取spider
+def spider_item_info(url):
+
+    html = requests.get(url).text
+    selector = etree.HTML(html)
+
+    title = selector.xpath('/html/head/title/text()')[0].strip()
+    price = selector.xpath('//span[@class="price_now"]/i/text()')[0]
+    add = selector.xpath('//div[@class="palce_li"]/span/i/text()')[0]
+    info = selector.xpath('//div[@class="baby_kuang clearfix"]/p/text()')[0]
+    data = {
+        'title':title,
+        'price':price,
+        'add':add,
+        'info':info,
+    }
+    print(data)
+
+url = 'http://zhuanzhuan.58.com/detail/837482115974103043z.shtml'
+spider_item_info(url)
