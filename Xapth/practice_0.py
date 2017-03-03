50cbf15a6b6ba0c6a6f0493f01a96121c9583cb7
Marmot93 <654332905@qq.com>
2017-03-03 21:42:09 +0800

    xpath重构详细信息抓取爬虫

---
+++ Xapth/practice_0.py
@@ -0,0 +1,25 @@
+import requests
+from lxml import etree
+from multiprocessing import Pool
+
+
+url_list = ['http://tieba.baidu.com/p/3522395718?pn={}'.format(i) for i in range(1,797)]
+
+
+def get_info(url):
+
+    web_data = requests.get(url).text
+    selector = etree.HTML(web_data)
+
+    users = set(selector.xpath('//ul[@class="p_author"]/li[3]/a/text()'))
+    info = selector.xpath('//*[@class="post_bubble_middle"]/text()')
+    print(users)
+    print(info)
+
+
+
+if __name__ == '__main__':
+    pool = Pool()
+    pool.map(get_info,url_list)
+    pool.close()
+    pool.join()
